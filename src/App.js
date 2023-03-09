3c642c2afa3b89df715f6a53c8187c051878b10e
Hummmuss <90830731+Hummmuss@users.noreply.github.com>
2023-03-10 00:00:37 +0300

    Основа база

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      тут когда нибудь будет курсач.
+    </div>
+  );
+}
+
+export default App;
