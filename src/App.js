5464d114757707915f930ea8f428fe56c0941324
Hummmuss <90830731+Hummmuss@users.noreply.github.com>
2023-03-13 22:44:17 +0300

    Эксперимент удался

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Sidebar from "./Components/Sidebar"
 
 function App() {
   return (
     <div className="App">
-      <sidebar />
-      aaaaa
+      <Sidebar />
     </div>
   );
 }
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,44 @@
+import react, {Component} from 'react'
+import { FormControl, Navbar, Nav, NavbarBrand, NavLink, Container, Form, Button} from 'react-bootstrap'
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse'
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle'
+import logo from './logo100.png'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default class Sidebar extends Component{
+    render() {
+        return (
+               <Navbar collapseOnSelect expand="md" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand >
+                        <img
+                        src={logo}
+                        height='53'
+                        width='75'
+                        className="d-inline-block align-top"
+                        alt = "Logo"
+                        />
+
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav"  > 
+                    <Nav>
+                        <NavLink href="/rec"> Reccomended </NavLink>
+                        <NavLink href="/prof"> Profile </NavLink>
+                    </Nav>
+                    <div width = "600">
+                    <Form inline class="m-auto">
+                        <FormControl
+                            type="text"
+                            placeholder="Search"
+                        />
+                  </Form>
+                    <Button variant="outline-secondary" class="m-auto">Search</Button>
+                    </div>
+                    </Navbar.Collapse>
+                </Container>
+               </Navbar>
+        )
+
+    }
+}
