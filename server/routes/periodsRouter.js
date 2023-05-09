462ddbe2a54c543d29d1ffb4c3ad29cc9109e462
firef <firefly.goo5@gmail.com>
2023-05-10 01:10:51 +0300

    верни алименты4

--- server/routes/periodsRouter.js
+++ server/routes/periodsRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const periodsController = require('../controllers/periodsController')
+const checkRole = require("../middleware/checkRoleMiddleware");
 
-router.post('/', periodsController.create)
+router.post('/', checkRole('ADMIN'), periodsController.create)
 router.get('/', periodsController.getAll)
 
 module.exports = router
