2c5d2cbc52dd41895d1b02111ec9a3c988886726
Hummmuss <90830731+Hummmuss@users.noreply.github.com>
2023-05-10 20:21:17 +0300

    Revert "111"
    This reverts commit 55673e4176e91d11b1f44c32b5622b53d46a1a27.

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -3,15 +3,12 @@ import {Routes, Route, Navigate} from 'react-router-dom'
 import {authRoutes, publicRoutes} from "../routes";
 import Recommendations from "../pages/Recommendations";
 import {RECOMMENDATIONS_ROUTE} from "../utils/consts";
-import React, {useContext} from 'react';
 
 const AppRouter = () => {
-    const {user} = useContext(Context)
-
-    console.log(user)
+    const isAuth = false
     return (
         <Routes>
-            {user.isAuth && authRoutes.map(({path, Component}) =>
+            {isAuth && authRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} element={<Component/>}  exact/>
                 )}
             {publicRoutes.map(({path, Component}) =>
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createContext } from 'react';
-import UserWebsite from "./website/UserWebsite";
 
-export const Context = createContext(null)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <Context.Provider value={{
-        user: new UserWebsite()
-    }}>
-        <App />
-    </Context.Provider>,
-
+    <App />
 )
 
 // If you want to start measuring performance in your app, pass a function
