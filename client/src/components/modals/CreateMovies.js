a8abc3c6dbc4b96c3d4b2fe4ad9d4d1ff2f9f468
firef <firefly.goo5@gmail.com>
2023-05-18 16:42:02 +0300

    лови ошибки снова хахахаха

---
+++ client/src/components/modals/CreateMovies.js
@@ -0,0 +1,113 @@
+import React, {useContext, useState} from 'react';
+import {Button, Col, Dropdown, Form, FormControl, Modal, Row} from "react-bootstrap";
+import {Context} from "../../index";
+import DropdownMenu from "react-bootstrap/DropdownMenu";
+
+const CreateMovies = (show, onHide) => {
+    const {movies} = useContext(Context)
+    const [info, setInfo] = useState([])
+    const addInfo = () =>{
+        setInfo([...info,{title:'', description:'', number: Date.now()}])
+    }
+
+    const removeInfo = (number) =>{
+        setInfo(info.filter(i => i.number !==number))
+    }
+
+    return (
+        <Modal
+            show = {show}
+            onHide = {onHide}
+            size="lg"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Добавить фильм
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Dropdown  className="mt-2 mb-2">
+                        <Dropdown.Toggle>Выберите жанр </Dropdown.Toggle>
+                        <DropdownMenu>
+                            {movies.genres.map(genre =>
+                                <Dropdown.Item key = {genre.id}>{genre.name}</Dropdown.Item> //где выпадающее меню блин, оно как бы есть, но пустое
+                            )}
+                        </DropdownMenu>
+                    </Dropdown>
+                    <Dropdown className="mt-2 mb-2">
+                        <Dropdown.Toggle>Выберите страну </Dropdown.Toggle>
+                        <DropdownMenu>
+                            {movies.countries.map(countrie =>
+                                <Dropdown.Item key = {countrie.id}>{countrie.name}</Dropdown.Item>
+                            )}
+                        </DropdownMenu>
+                    </Dropdown>
+                    <Dropdown className="mt-2 mb-2">
+                        <Dropdown.Toggle>Выберите режиссера </Dropdown.Toggle>
+                        <DropdownMenu>
+                            {movies.directors.map(director =>
+                                <Dropdown.Item key = {director.id}>{director.name}</Dropdown.Item>
+                            )}
+                        </DropdownMenu>
+                    </Dropdown>
+                    <Dropdown  className="mt-2 mb-2" >
+                        <Dropdown.Toggle>Выберите год выпуска </Dropdown.Toggle>
+                        <DropdownMenu>
+                            {movies.periods.map(period =>
+                                <Dropdown.Item key = {period.id}>{period.name}</Dropdown.Item>
+                            )}
+                        </DropdownMenu>
+
+                    </Dropdown>
+                    <FormControl
+                        className="mt-3"
+                        placeholder="Введите название фильма"
+                    />
+                    <FormControl
+                        className="mt-3"
+                        placeholder="Введите название фильма"
+                        type = "file"
+                    />
+                    <hr/>
+                    <Button
+                        variant="outline-success"
+                        onClick={addInfo}
+                    >
+                        Добавить ?описание
+                    </Button>
+                    {info.map(i =>
+                            <Row className="mt-4" key = {i.number}> //???
+                                <Col md={4}>
+                                    <FormControl
+                                        placeholder="Введите название аЭЭЭЭЭЭ?"
+                                    />
+                                </Col>
+                                <Col md={4}>
+                                    <FormControl
+                                        placeholder="Введите описание"
+                                    />
+                                </Col>
+                                <Col md ={4}>
+                                    <Button
+                                        onClick={() => removeInfo((i.number))}
+                                        variant={"outline-danger"}
+                                    >
+                                        Удалить описание
+                                    </Button>
+                                </Col>
+                            </Row>
+                        )}
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
+                <Button variant="outline-success" onClick={onHide}>Добавить</Button>
+
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default CreateMovies;
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Container} from "react-bootstrap";
-import {useNavigate} from "react-router-dom";
+import CreateCountries from "../components/modals/CreateCountries";
+import CreateDirectors from "../components/modals/CreateDirectors";
+import CreateGenres from "../components/modals/CreateGenres";
+import CreatePeriods from "../components/modals/CreatePeriods";
+import CreateMovies from "../components/modals/CreateMovies";
 
 const Admin = () => {
+    const [countriesVisible, setCountriesVisible] = useState(false)
+    const [directorsVisible, setDirectorsVisible] = useState(false)
+    const [genresVisible, setGenresVisible] = useState(false)
+    const [periodsVisible, setPeriodsVisible] = useState(false)
+    const [moviesVisible, setMoviesVisible] = useState(false)
+
     return (
         <Container className="d-flex flex-column">
-            <Button variant={"outline-success"} className="mt-2">Добавить страну</Button>
-            <Button variant={"outline-success"} className="mt-2">Добавить режиссера</Button>
-            <Button variant={"outline-success"} className="mt-2">Добавить жанр</Button>
-            <Button variant={"outline-success"} className="mt-2">Добавить длительность фильма</Button>
+            <Button
+                variant={"outline-success"}
+                className="mt-4 p-2"
+                onClick={()=>setCountriesVisible(true)}
+            >
+                Добавить страну
+            </Button>
+            <Button
+                variant={"outline-success"}
+                className="mt-4 p-2"
+                onClick={()=>setDirectorsVisible(true)}
+            >
+                Добавить режиссера
+            </Button>
+            <Button
+                variant={"outline-success"}
+                className="mt-4 p-2"
+                onClick={()=>setGenresVisible(true)}
+            >
+                Добавить жанр
+            </Button>
+            <Button
+                variant={"outline-success"}
+                className="mt-4 p-2"
+                onClick={()=>setPeriodsVisible(true)}
+            >
+                Добавить длительность фильма
+            </Button>
 
+            <CreateMovies show ={moviesVisible} onHide = {() => setMoviesVisible(false)}/>
+            <CreateCountries show ={countriesVisible} onHide = {() => setCountriesVisible(false)}/>
+            <CreateDirectors show ={directorsVisible} onHide = {() => setDirectorsVisible(false)}/>
+            <CreateGenres show ={genresVisible} onHide = {() => setGenresVisible(false)}/>
+            <CreatePeriods show ={periodsVisible} onHide = {() => setPeriodsVisible(false)}/>
         </Container>
     );
 };
